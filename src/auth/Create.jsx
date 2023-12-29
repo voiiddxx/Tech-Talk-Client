0dda91a0537f8505e72b6b9ccef876172c57bbb7
voiiddxx <95859137+voiiddxx@users.noreply.github.com>
2023-12-29 17:49:53 +0530

    register ui implemented

--- src/auth/Create.jsx
+++ src/auth/Create.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import "./Create.css"
 
 const Create = () => {
   return (
-    <div>
-      <h1> this is account creation page</h1>
+   <>
+   <div className="create-main">
+    <div className="register-card">
+        <h1>Create Your Account</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus consequatur ratione aut? Itaque, voluptas corrupti.</p>
+        <br />
+       
+       <form>
+        <p className='title'>username</p>
+       <input type="text" placeholder='Username' />
+       <br />
+        <p className='title'>Email address</p>
+       <input type="text" placeholder='Username' />
+       <br />
+        <p className='title'>Your Password</p>
+       <input type="text" placeholder='Username' />
+
+       <div className="button">
+        <h2>Create Now</h2>
+       </div>
+
+       <h3>Already have an account? <span>Login Now</span></h3>
+       </form>
     </div>
+   </div>
+   </>
   )
 }
 
